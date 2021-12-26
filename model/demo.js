99e782b847a0f8222784df0f87cb91606ca689a0
Vishal Codez <78276141+VishalCodez@users.noreply.github.com>
2021-12-26 06:10:33 +0530

    Update demo.js

--- model/demo.js
+++ model/demo.js
@@ -8,10 +8,7 @@ const Demo = model("Demo", demoSchema);
 
 module.exports = Demo;
 /**
- * @INFO
  * Coded By Vishal Codez
- * @INFO
  * Please Don't Forget To Give Credits To Vishal Codez if You Are Using This
- * @INFO
  * Please Mention Vishal Codez / In Code Streakers, When Using This Code!
  */
