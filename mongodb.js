513fab884ccb7fff9a98a811403bfba8069be436
Vishal Codez <78276141+VishalCodez@users.noreply.github.com>
2021-12-26 06:11:00 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -7,10 +7,7 @@ mongoose.connect(`${process.env.MONGODB_SRV}`, (err) => {
     : Logger.info("connected to the database successfully");
 });
 /**
- * @INFO
  * Coded By Vishal Codez
- * @INFO
  * Please Don't Forget To Give Credits To Vishal Codez if You Are Using This
- * @INFO
  * Please Mention Vishal Codez / In Code Streakers, When Using This Code!
  */
